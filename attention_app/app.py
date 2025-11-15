03b3aa24f13dbdbe5d6807675c02e7dfe4232844
Ana Pinto <anapinto22012003@gmail.com>
2025-11-15 20:40:17 +0000

    README added

--- attention_app/app.py
+++ attention_app/app.py
@@ -1,10 +1,14 @@
 """Construct the Shiny application instance."""
 
+from pathlib import Path
+
 from shiny import App
 
 from .server import server
 from .ui import app_ui
 
-app = App(app_ui, server)
+STATIC_PATH = Path(__file__).resolve().parent.parent / "static"
+
+app = App(app_ui, server, static_assets=STATIC_PATH)
 
 __all__ = ["app"]
